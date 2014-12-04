f40bde048590a0e6357a3e4453a016e63339670d
Pierre de Buyl <pdebuyl@ulb.ac.be>
2014-12-04 15:17:48 +0100

    pep8-ifying and pyflaking

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from setuptools import setup
-from os import path
 
 import sftmpl
 
 setup(name="sftmpl",
       version=sftmpl.__version__,
       description="Single file templater",
-      long_desc = open('README.rst','r').read(),
+      long_desc=open('README.rst', 'r').read(),
       author=sftmpl.__author__,
       author_email="pdebuyl at sign pdebuyl dot be",
       license="BSD",
       url="http://github.com/pdebuyl/sftmpl",
-      packages = ['sftmpl'],
+      packages=['sftmpl'],
       entry_points={
           'console_scripts': [
               'sftmpl=sftmpl:main',
           ],
       },
 )
-
